b6bc24494def3f2c532f41f57bd7c1b7a9a481ea
Achraf aamri <Achraf.aamri@gmail.com>
2020-05-27 18:25:58 +0000

    React-dates daterangepicker demo
    React-dates daterangepicker demo responsive

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# daterange-picker-react-example
+
+Date range picker react example ( using Airbnb react-dates )
+
+Check My Blog for th tutorial [https://achrafaamri.com/](https://achrafaamri.com/)
 
 ## Available Scripts
 
@@ -36,33 +40,3 @@ If you aren’t satisfied with the build tool and configuration choices, you can
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "react-dates/initialize";
+import { DateRangePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+import Responsive from "react-responsive";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    startDate: null,
+    endDate: null,
+    endDateFormatted: null,
+    startDateFormatted: null,
+  };
+  hundleDateChange(startDate, endDate) {
+    this.setState(() => ({
+      endDate,
+      startDate,
+    }));
+    if (startDate != null) {
+      this.setState(() => ({
+        startDateFormatted: startDate.format("D-MM-Y"),
+      }));
+    }
+    if (endDate != null) {
+      this.setState(() => ({
+        endDateFormatted: endDate.format("D-MM-Y"),
+      }));
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>React DateRangePicker</h2>
+        <pre style={{ textAlign: "left" }}>
+          <p> start date : {JSON.stringify(this.state.startDate)} </p>
+          <p> start date Formatted: {this.state.startDateFormatted} </p>
+          <p> end date : {JSON.stringify(this.state.endDate)} </p>
+          <p> end date Formatted : {this.state.endDateFormatted} </p>
+        </pre>
+
+        <Mobile>
+          <DateRangePicker
+            startDate={this.state.startDate}
+            startDateId="start_date_id"
+            endDate={this.state.endDate}
+            endDateId="end_date_id"
+            onDatesChange={({ startDate, endDate }) =>
+              this.hundleDateChange(startDate, endDate)
+            }
+            focusedInput={this.state.focusedInput}
+            onFocusChange={(focusedInput) => this.setState({ focusedInput })}
+            numberOfMonths={1}
+            orientation="horizontal"
+          />
+        </Mobile>
+
+        <Default>
+          <DateRangePicker
+            startDate={this.state.startDate}
+            startDateId="start_date_id"
+            endDate={this.state.endDate}
+            endDateId="end_date_id"
+            onDatesChange={({ startDate, endDate }) =>
+              this.hundleDateChange(startDate, endDate)
+            }
+            focusedInput={this.state.focusedInput}
+            onFocusChange={(focusedInput) => this.setState({ focusedInput })}
+            numberOfMonths={2}
+            orientation="horizontal"
+          />
+        </Default>
+      </div>
+    );
+  }
 }
-
 export default App;
+
+export const Mobile = (props) => <Responsive {...props} maxWidth={767} />;
+export const Default = (props) => <Responsive {...props} minWidth={768} />;
